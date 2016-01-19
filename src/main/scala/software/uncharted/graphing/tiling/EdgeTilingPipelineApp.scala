297c52c9ce65a2b5aa23967b2105fd713aa2927e
Nathan Kronenfeld <nkronenfeld@uncharted.software>
2016-01-19 17:10:14 -0500

    First pass at pipeline app for edge tiling of graphs

---
+++ src/main/scala/software/uncharted/graphing/tiling/EdgeTilingPipelineApp.scala
@@ -0,0 +1,94 @@
+/**
+  * Copyright © 2014-2016 Uncharted Software Inc. All rights reserved.
+  *
+  * Property of Uncharted™, formerly Oculus Info Inc.
+  * http://uncharted.software/
+  *
+  * This software is the confidential and proprietary information of
+  * Uncharted Software Inc. ("Confidential Information"). You shall not
+  * disclose such Confidential Information and shall use it only in
+  * accordance with the terms of the license agreement you entered into
+  * with Uncharted Software Inc.
+  */
+package software.uncharted.graphing.tiling
+
+import java.io.FileInputStream
+import java.util.Properties
+
+import com.oculusinfo.tilegen.datasets.TilingTaskParameters
+import com.oculusinfo.tilegen.pipeline.{Bounds, PipelineStage, HBaseParameters}
+import com.oculusinfo.tilegen.util.{ArgumentParser, PropertiesWrapper, KeyValueArgumentSource}
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.SQLContext
+
+
+object EdgeTilingPipelineApp {
+  import com.oculusinfo.tilegen.pipeline.PipelineOperations._
+  import GraphOperations._
+
+  private def getKVFile (fileName: String):KeyValueArgumentSource = {
+    val props = new Properties()
+    val propsFile = new FileInputStream(fileName)
+    props.load(propsFile)
+    propsFile.close()
+    new PropertiesWrapper(props)
+  }
+
+  def main (args: Array[String]): Unit = {
+    // Reduce log clutter
+    Logger.getRootLogger.setLevel(Level.WARN)
+
+    val argParser = new ArgumentParser(args)
+
+    val sc = new SparkContext()
+    val sqlc = new SQLContext(sc)
+
+    try {
+      val base = argParser.getString("base", "The base location of graph layout information")
+      val levels = argParser.getIntSeq("levels",
+        """The number of levels per hierarchy level.  These will be applied in reverse order -
+          | so that a value of "4,3,2" means that hierarchy level 2 will be used for tiling
+          | levels 0-3, hierarcy level 1 will be used for tiling levels 4-6, and hierarchy
+          | level 0 will be used for tiling levels 7 and 8.""".stripMargin)
+      val edgeFileDescriptor = argParser.getString("edgeColumns", "A file containing parse information for the edge lines")
+      val tileSet = argParser.getString("name", "The name of the edge tile set to produce")
+      val tileDesc = argParser.getStringOption("desc", "A description of the edge tile set to produce")
+      val prefix = argParser.getStringOption("prefix", "A prefix to prepend to the tile set name")
+      val x1Col = argParser.getString("x1", """The column to use for the X value of the source of each edge (defaults to "x1")""", Some("x1"))
+      val y1Col = argParser.getString("y1", """The column to use for the Y value of the source of each edge (defaults to "y1")""", Some("y1"))
+      val x2Col = argParser.getString("x2", """The column to use for the X value of the destination of each edge (defaults to "x2")""", Some("x2"))
+      val y2Col = argParser.getString("y2", """The column to use for the Y value of the destination of each edge (defaults to "y2")""", Some("y2"))
+      val zkq = argParser.getString("zkq", "zookeeper quorum", Some("uscc0-master0.uncharted.software"))
+      val zkp = argParser.getString("zkp", "zookeeper port", Some("2181"))
+      val zkm = argParser.getString("xkm", "zookeeper master", Some("uscc0-master0.uncharted.software:60000"))
+      val epsilon = 1E-16
+
+
+      // List of ((min tiling level, max tiling level), graph hierarchy level)
+      val clusterAndGraphLevels = levels.scanLeft(0)(_ + _).sliding(2).map(bounds => (bounds(0), bounds(1) - 1)).toList.reverse.zipWithIndex.reverse
+      val bounds = Bounds(0.0, 0.0, 256.0 - epsilon, 256.0 - epsilon)
+      val hbaseParameters = Some(HBaseParameters(zkq, zkp, zkm))
+
+      clusterAndGraphLevels.foreach { case ((minT, maxT), g) =>
+        val tilingParameters = new TilingTaskParameters(
+          tileSet, tileDesc.getOrElse(""), prefix,
+          Seq((minT to maxT).toSeq), 256, 256, None, None)
+
+        val loadStage = PipelineStage("load level  " + g, loadCsvDataOp(base + "/level_" + g, getKVFile(edgeFileDescriptor))(_))
+        val tilingStage = PipelineStage("Tiling level " + g,
+          segmentTilingOp(x1Col, y1Col, x2Col, y2Col, Some(bounds), tilingParameters, hbaseParameters)(_)
+        )
+
+      }
+
+
+      sc.stop
+    } catch {
+      case e: Exception => {
+        e.printStackTrace()
+        argParser.usage
+      }
+    }
+  }
+}
---
+++ src/main/scala/software/uncharted/graphing/tiling/GraphOperations.scala
@@ -0,0 +1,145 @@
+package software.uncharted.graphing.tiling
+
+import com.oculusinfo.binning.{BinIndex, TileIndex}
+import com.oculusinfo.tilegen.datasets.{StandardScalingFunctions, TilingTask, LineDrawingType, TilingTaskParameters}
+import com.oculusinfo.tilegen.pipeline.{Bounds, PipelineOperations, PipelineData, HBaseParameters}
+import com.oculusinfo.tilegen.pipeline.OperationType._
+import com.oculusinfo.tilegen.tiling.{StandardBinningFunctions, TileIO, LocalTileIO, HBaseTileIO}
+
+import scala.collection.mutable.{Map => MutableMap}
+import scala.reflect.ClassTag
+
+/**
+  * Created by nkronenfeld on 2016-01-19.
+  */
+object GraphOperations {
+
+  import PipelineOperations._
+
+  // Implicit for converting a scala map to a java properties object
+  implicit def map2Properties(map: Map[String, String]): java.util.Properties = {
+    (new java.util.Properties /: map) { case (props, (k, v)) => props.put(k, v); props }
+  }
+
+  def segmentTilingOp(x1ColSpec: String,
+                      y1ColSpec: String,
+                      x2ColSpec: String,
+                      y2ColSpec: String,
+                      bounds: Option[Bounds],
+                      tilingParams: TilingTaskParameters,
+                      hbaseParameters: Option[HBaseParameters],
+                      operation: OperationType = COUNT,
+                      valueColSpec: Option[String] = None,
+                      valueColType: Option[String] = None,
+                      lineType: Option[LineDrawingType] = Some(LineDrawingType.Lines),
+                      minimumSegmentLength: Option[Int] = Some(4),
+                      maximumSegmentLength: Option[Int] = Some(1024),
+                      maximumLeaderLength: Option[Int] = Some(1024))
+                     (input: PipelineData) = {
+    val tileIO = hbaseParameters match {
+      case Some(p) => new HBaseTileIO(p.zookeeperQuorum, p.zookeeperPort, p.hbaseMaster)
+      case None => new LocalTileIO("avro")
+    }
+
+    val projection = bounds match {
+      case Some(b) => Map(
+        "oculus.binning.projection.type" -> "areaofinterest",
+        "oculus.binning.projection.autobounds" -> "false",
+        "oculus.binning.projection.minX" -> b.minX.toString,
+        "oculus.binning.projection.minY" -> b.minY.toString,
+        "oculus.binning.projection.maxX" -> b.maxX.toString,
+        "oculus.binning.projection.maxY" -> b.maxY.toString)
+      case None => Map(
+        "oculus.binning.projection.type" -> "areaofinterest",
+        "oculus.binning.projection.autobounds" -> "true")
+    }
+    segmentTilingOpImpl(
+      x1ColSpec, y1ColSpec, x2ColSpec, y2ColSpec, operation, valueColSpec, valueColType,
+      lineType, minimumSegmentLength, maximumSegmentLength, maximumLeaderLength,
+      tilingParams, tileIO, projection)(input)
+  }
+
+  private def segmentTilingOpImpl(x1ColSpec: String,
+                                  y1ColSpec: String,
+                                  x2ColSpec: String,
+                                  y2ColSpec: String,
+                                  operation: OperationType,
+                                  valueColSpec: Option[String],
+                                  valueColType: Option[String],
+                                  lineType: Option[LineDrawingType] = Some(LineDrawingType.Lines),
+                                  minimumSegmentLength: Option[Int] = Some(4),
+                                  maximumSegmentLength: Option[Int] = Some(1024),
+                                  maximumLeaderLength: Option[Int] = Some(1024),
+                                  taskParameters: TilingTaskParameters,
+                                  tileIO: TileIO,
+                                  projection: Map[String, String])
+                                 (input: PipelineData) = {
+    // Populate baseline args
+    val args: Map[String, String] = Map(
+      "oculus.binning.name" -> taskParameters.name,
+      "oculus.binning.description" -> taskParameters.description,
+      "oculus.binning.tileWidth" -> taskParameters.tileWidth.toString,
+      "oculus.binning.tileHeight" -> taskParameters.tileHeight.toString,
+      "oculus.binning.index.type" -> "cartesian",
+      "oculus.binning.index.field.0" -> x1ColSpec,
+      "oculus.binning.index.field.1" -> y1ColSpec,
+      "oculus.binning.index.field.2" -> x2ColSpec,
+      "oculus.binning.index.field.3" -> y2ColSpec)
+
+    val valueProps = operation match {
+      case SUM | MAX | MIN | MEAN =>
+        Map("oculus.binning.value.type" -> "field",
+          "oculus.binning.value.field" -> valueColSpec.get,
+          "oculus.binning.value.valueType" -> valueColType.get,
+          "oculus.binning.value.aggregation" -> operation.toString.toLowerCase,
+          "oculus.binning.value.serializer" -> s"[${valueColType.get}]-a")
+      case _ =>
+        Map("oculus.binning.value.type" -> "count",
+          "oculus.binning.value.valueType" -> "int",
+          "oculus.binning.value.serializer" -> "[int]-a")
+    }
+
+    // Parse bounds and level args
+    val levelsProps = createLevelsProps("oculus.binning", taskParameters.levels)
+
+    val tableName = PipelineOperations.getOrGenTableName(input, "heatmap_op")
+
+    val tilingTask = TilingTask(input.sqlContext, tableName, args ++ levelsProps ++ valueProps ++ projection)
+
+    def withValueType[PT: ClassTag](task: TilingTask[PT, _, _, _]): PipelineData = {
+      val (locateFcn, populateFcn): (Traversable[Int] => Seq[Any] => Traversable[(TileIndex, Array[BinIndex])],
+        (TileIndex, Array[BinIndex], PT) => MutableMap[BinIndex, PT]) =
+        lineType match {
+          case Some(LineDrawingType.LeaderLines) =>
+            (
+              StandardBinningFunctions.locateLineLeaders(task.getIndexScheme, task.getTilePyramid,
+                minimumSegmentLength, maximumLeaderLength.getOrElse(1024),
+                task.getNumXBins, task.getNumYBins),
+              StandardBinningFunctions.populateTileWithLineLeaders(maximumLeaderLength.getOrElse(1024),
+                StandardScalingFunctions.identityScale)
+              )
+
+          case Some(LineDrawingType.Arcs) =>
+            (
+              StandardBinningFunctions.locateArcs(task.getIndexScheme, task.getTilePyramid,
+                minimumSegmentLength, maximumLeaderLength,
+                task.getNumXBins, task.getNumYBins),
+              StandardBinningFunctions.populateTileWithArcs(maximumLeaderLength,
+                StandardScalingFunctions.identityScale)
+              )
+
+          case Some(LineDrawingType.Lines) | _ =>
+            (
+              StandardBinningFunctions.locateLine(task.getIndexScheme, task.getTilePyramid,
+                minimumSegmentLength, maximumSegmentLength, task.getNumXBins, task.getNumYBins),
+              StandardBinningFunctions.populateTileWithLineSegments(StandardScalingFunctions.identityScale)
+              )
+        }
+      task.doParameterizedTiling(tileIO, locateFcn, populateFcn)
+      PipelineData(input.sqlContext, input.srdd, Option(tableName))
+    }
+    withValueType(tilingTask)
+
+    PipelineData(input.sqlContext, input.srdd, Option(tableName))
+  }
+}
