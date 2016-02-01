1d72a8d47918b2c0537e5c51eecf53d49ecfc818
Nathan Kronenfeld <nkronenfeld@uncharted.software>
2016-01-31 22:03:18 -0500

    Fix some tiling scripts

--- src/scripts/affinity-graph-edge-tiling.sh
+++ src/scripts/affinity-graph-edge-tiling.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MAIN_CLASS=software.uncharted.graphing.tiling.NodeTilingPipelineApp
+MAIN_CLASS=software.uncharted.graphing.tiling.EdgeTilingPipelineApp
 MAIN_JAR=../lib/xdata-graph.jar
 JOB_MASTER=yarn-client
 HDFS_LOC=hdfs://uscc0-master0.uncharted.software
@@ -17,6 +17,10 @@ spark-submit \
 	${MAIN_JAR} \
 	-base ${HDFS_LOC}/user/nkronenfeld/affinity-graph/ag-layout/ \
 	-levels 3,2,2,2,2,2 \
-	-nodeColumns affinity-graph-node-tiling.bd affinity-graph-node-tiling.bd \
-	-name affinity-graph-test-nodes \
-	-nodeTest "^node.*"
+	-edgeColumns affinity-graph-edge-tiling.bd affinity-graph-edge-tiling.bd \
+	-name affinity-graph-test-edges-intra \
+	-minLength 4 \
+	-maxLength 1024 \
+	-edgeTest "^edge.*" \
+	-edgeColumn edgeType \
+	-edgeType intra
--- src/scripts/pull-table.sh
+++ src/scripts/pull-table.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-MAIN_CLASS=software.uncharted.graphing.tiling.NodeTilingPipelineApp
+MAIN_CLASS=software.uncharted.graphing.tiling.TilesetPuller
 MAIN_JAR=../lib/xdata-graph.jar
 JOB_MASTER=yarn-client
 HDFS_LOC=hdfs://uscc0-master0.uncharted.software
+TABLE=$1
+FILE=$2
 
 spark-submit \
-	--num-executors 10 \
-	--executor-memory 10g \
-	--executor-cores 4 \
+	--num-executors 1 \
+	--driver-memory 2g \
+	--executor-cores 1 \
     --conf spark.executor.extraClassPath=/opt/cloudera/parcels/CDH/lib/hbase/lib/htrace-core-3.1.0-incubating.jar \
     --driver-class-path /opt/cloudera/parcels/CDH/lib/hbase/lib/htrace-core-3.1.0-incubating.jar \
     --jars /opt/cloudera/parcels/CDH/lib/hbase/lib/htrace-core-3.1.0-incubating.jar \
 	--class ${MAIN_CLASS} \
 	--master ${JOB_MASTER} \
 	${MAIN_JAR} \
-	-base ${HDFS_LOC}/user/nkronenfeld/affinity-graph/ag-layout/ \
-	-levels 3,2,2,2,2,2 \
-	-nodeColumns affinity-graph-node-tiling.bd affinity-graph-node-tiling.bd \
-	-name affinity-graph-test-nodes \
-	-nodeTest "^node.*"
+	-table ${TABLE} \
+	-file ${FILE} \
+	-config /etc/hbase/conf/hbase-site.xml
