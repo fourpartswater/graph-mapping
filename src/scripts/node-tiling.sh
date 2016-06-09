1dc0ae677b32e1db658128b463126ca55f518f74
Nathan Kronenfeld <nkronenfeld@uncharted.software>
2016-06-09 14:26:41 -0400

    Attempt to update node tiling to the latest configuration paradigm.  This is a test version only.

--- src/scripts/node-tiling.sh
+++ src/scripts/node-tiling.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+# Set up default parameters
 MAIN_JAR=../xdata-graph-0.1-SNAPSHOT/lib/xdata-graph.jar
-MAIN_CLASS=software.uncharted.graphing.salt.NodeTilingPipeline
 BASE_LOCATION=/user/nkronenfeld/graphing/timing
-
 TOP_LEVEL=3
 NEXT_LEVELS=2
-DATASET=
 
+# Set up application-specific parameters
+# MAIN_CLASS=software.uncharted.graphing.salt.NodeTilingPipeline
+MAIN_CLASS=software.uncharted.graphing.config.ConfigurationTester
+APPLICATION_NAME="Node tiling pipeline"
+
+# Set up our dataset
+DATASET=
 while [ "$1" != "" ]; do
 	case $1 in 
 		-d | --dataset )
@@ -31,39 +36,40 @@ if [ "${DATASET}" == "" ]; then
 	exit
 fi
 
+
+
+# copy our output script into the dataset, if it's not already there
+if [ ! -e ${DATASET}/output.conf ]; then
+	cp scripts/config/default-output.conf ${DATASET}/output.conf
+fi
+
+# move to where our dataset is stored
 pushd ${DATASET}
 
+
+
+
 echo
+echo Running ${APPLICATION_NAME}
 echo Running in `pwd`
 echo Starting at `date`
 
-MAX_LEVEL=`hdfs dfs -ls ${BASE_LOCATION}/${DATASET}/layout | awk '{print $8}' | awk -F / '{print $NF}' | grep level_ | awk -F'_' '{print $2}' | sort -nr | head -n1`
-PARTITIONS=`hdfs dfs -ls ${BASE_LOCATION}/${DATASET}/layout/level_0 | wc -l`
-EXECUTORS=$(expr $(expr ${PARTITIONS} + 7) / 8)
-LEVELS=
-for level in `hdfs dfs -ls ${BASE_LOCATION}/${DATASET}/layout | awk '{print $8}' | awk -F '/' '{print $NF}' | grep level_ | awk -F '_' '{print $2}' | sort`
-do
-	if [ $level -eq 0 ]; then
-		curLevels=${TOP_LEVEL}
-	else
-		curLevels=${NEXT_LEVELS}
-	fi
-	if [ -z ${LEVELS} ]; then
-		LEVELS=${curLevels}
-	else
-		LEVELS=${LEVELS},${curLevels}
-	fi
-done
+# Determine parameters about our dataset
+MAX_LEVEL=$(getMaxLevel ${DATASET})
+PARTITIONS=$(getPartitiosn ${DATASET})
+EXECUTORS=$(getExecutors ${DATASET})
+LEVELS=($(hardCodedLevels ${DATASET} ${TOP_LEVEL} ${NEXT_LEVELS}))
 DATATABLE="graph-nodes-${DATASET}-salt"
 
-OTHER_ARGS="-nodeTest ^node.*"
+OTHER_ARGS=
 
 case ${DATASET} in 
 
 	analytics)
-		OTHER_ARGS="${OTHER_ARGS} -analytic software.uncharted.graphing.analytics.SumAnalytic3"
-		OTHER_ARGS="${OTHER_ARGS} -analytic software.uncharted.graphing.analytics.MeanAnalytic4"
-		OTHER_ARGS="${OTHER_ARGS} -analytic software.uncharted.graphing.analytics.MinAnalytic5"
+		ANALYTICS=
+		OTHER_ARGS="${OTHER_ARGS} -Dgraph.analytics.0=software.uncharted.graphing.analytics.SumAnalytic3"
+		OTHER_ARGS="${OTHER_ARGS} -Dgraph.analytics.1=software.uncharted.graphing.analytics.MeanAnalytic4"
+		OTHER_ARGS="${OTHER_ARGS} -Dgraph.analytics.2=software.uncharted.graphing.analytics.MinAnalytic5"
 		;;
 
 esac
@@ -87,9 +93,23 @@ echo "drop '${DATATABLE}'" >> clear-hbase-table
 
 hbase shell < clear-hbase-table
 
+
+
+# See if there is a local node configuration file
+if [ -e node-tiling.conf ]; then
+	CONFIGURATION=node-tiling.conf
+else
+	CONFIGURATION=
+fi
+
+
+
 # Extra jars needed by tiling processes
 EXTRA_JARS=/opt/cloudera/parcels/CDH/lib/hbase/lib/htrace-core-3.2.0-incubating.jar:/opt/cloudera/parcels/CDH/lib/hbase/lib/hbase-client-1.0.0-cdh5.5.2.jar
 
+
+
+# OK, that's all we need - start tiling.
 STARTTIME=$(date +%s)
 echo Starting tiling
 
@@ -102,12 +122,11 @@ echo spark-submit \
     --jars ${EXTRA_JARS} \
 	--class ${MAIN_CLASS} \
 	${MAIN_JAR} \
-	-base ${BASE_LOCATION}/${DATASET}/layout/ \
-	-levels ${LEVELS} \
-	-name ${DATATABLE} \
-	-hbaseConfig /etc/hbase/conf/hbase-site.xml \
+	output.conf \
+	${CONFIGURATION} \
+	-Dtiling.source=$( relativeToSource ${DATASET} layout ) \
+	$( getLevelConfig ${LEVELS[@]} ) \
 	${OTHER_ARGS} >> node-tiling.log
-
 
 spark-submit \
 	--num-executors ${EXECUTORS} \
@@ -118,11 +137,13 @@ spark-submit \
     --jars ${EXTRA_JARS} \
 	--class ${MAIN_CLASS} \
 	${MAIN_JAR} \
-	-base ${BASE_LOCATION}/${DATASET}/layout/ \
-	-levels ${LEVELS} \
-	-name ${DATATABLE} \
-	-hbaseConfig /etc/hbase/conf/hbase-site.xml \
-	${OTHER_ARGS} |& tee -a node-tiling.log
+	output.conf \
+	${CONFIGURATION} \
+	-Dtiling.source=$( relativeToSource ${DATASET} layout ) \
+	$( getLevelConfig ${LEVELS[@]} ) \
+	${OTHER_ARGS} >> node-tiling.log
+
+
 
 ENDTIME=$(date +%s)
 
