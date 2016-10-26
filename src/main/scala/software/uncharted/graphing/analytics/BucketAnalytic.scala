9ee951eeae57595e95581eb650ba4df605af4435
Philippe Horne <phorne@uncharted.software>
2016-10-26 16:59:39 -0400

    Bucket analytics now allows user defined buckets.

--- src/main/scala/software/uncharted/graphing/analytics/BucketAnalytic.scala
+++ src/main/scala/software/uncharted/graphing/analytics/BucketAnalytic.scala
@@ -15,18 +15,21 @@ package software.uncharted.graphing.analytics
 import software.uncharted.salt.core.analytic.Aggregator
 import com.typesafe.config.Config
 
-class BucketAnalytic (c: Int, minValue: Double, maxValue: Double, bins: Int) extends CustomGraphAnalytic[Array[Int]] {
+class BucketAnalytic (c: Int, bins: Array[Bucket]) extends CustomGraphAnalytic[Array[Int]] {
   val analyticKey = "analytic"
   val bucketKey = "bucket"
   val columnKey = "column"
   val minValueKey = "minValue"
   val maxValueKey = "maxValue"
   val binsKey = "bins"
+  val indexKey = "index"
+  val bucketTypeKey = "type"
+  val userBucketListKey = "bucket"
 
 
   override val name: String = s"bucket column $c"
 
-  def this() = this(0, 0, 0, 0)
+  def this() = this(0, new Array[Bucket](0))
 
   /**
     * Take two processed, aggregated values, and determine the maximum value of the pair.
@@ -49,7 +52,7 @@ class BucketAnalytic (c: Int, minValue: Double, maxValue: Double, bins: Int) ext
   override val column: Int = c
   override val aggregator: Aggregator[String, Array[Int], String] =
     new WrappingClusterAggregator[Double, Array[Int], Array[Int]](
-      new BucketAggregator(minValue, maxValue, bins),
+      new BucketAggregator(bins),
       (input: String) => input.toDouble,
       (output: Array[Int]) => output.mkString(",")
     )
@@ -60,19 +63,82 @@ class BucketAnalytic (c: Int, minValue: Double, maxValue: Double, bins: Int) ext
     * @return Configured instance
     */
   override def initialize(configs: Config): CustomGraphAnalytic[Array[Int]] = {
+
+    //Buckets can be either user defined or equal size. Equal size buckets have their limits calculated.
+    //Expected formats:
+//    analytic {
+//      bucket {
+//        type = "user-defined"
+//        column = 2
+//        bucket: [
+//        {
+//          index = 0
+//          minValue = 0
+//          maxValue = 24
+//        },
+//        {
+//          index = 1
+//          minValue = 25
+//          maxValue = 49
+//        },
+//        {
+//          index = 2
+//          minValue = 50
+//          maxValue = 74
+//        },
+//        {
+//          index = 3
+//          minValue = 75
+//          maxValue = 99
+//        }
+//        ]
+//      }
+//    }
+//
+//    analytic {
+//      bucket {
+//        type = "equal-size"
+//        column = 2
+//        minValue = 0
+//        maxValue = 100
+//        bins = 4
+//      }
+//    }
+
     val analyticConfig = configs.getConfig(analyticKey)
     val bucketConfig = analyticConfig.getConfig(bucketKey)
+    val bucketType = bucketConfig.getString(bucketTypeKey)
     val c = bucketConfig.getInt(columnKey)
-    val minValue = bucketConfig.getDouble(minValueKey)
-    val maxValue = bucketConfig.getDouble(maxValueKey)
-    val bins = bucketConfig.getInt(binsKey)
 
-    new BucketAnalytic(c, minValue, maxValue, bins)
+    bucketType match {
+      case "equal-size" =>
+        //Every bucket is of equal size over the whole window defined in the config file.
+        val minValue = bucketConfig.getDouble(minValueKey)
+        val maxValue = bucketConfig.getDouble(maxValueKey)
+        val binCount = bucketConfig.getInt(binsKey)
+
+        val interval = ((maxValue - minValue) / binCount).ceil.toInt
+        val buckets = new Array[Bucket](binCount)
+        for ( i <- 0 to (buckets.length - 1)) {
+          buckets(i) = new Bucket(minValue + i * interval, minValue + (i + 1) * interval - 1, i)
+        }
+
+        new BucketAnalytic(c, buckets)
+
+      case "user-defined" =>
+        //Series of buckets defined by the user in the config file.
+        val binConfig = bucketConfig.getConfigList(userBucketListKey)
+        val buckets = binConfig.toArray().map(c => c.asInstanceOf[Config])
+          .map(b => new Bucket(b.getDouble(minValueKey), b.getDouble(maxValueKey), b.getInt(indexKey)))
+
+        new BucketAnalytic(c, buckets)
+      case _ => this
+    }
   }
 }
 
-class BucketAggregator (minValue: Double, maxValue: Double, bins: Int) extends Aggregator[Double, Array[Int], Array[Int]] {
-  override def default(): Array[Int] = Array.fill(bins)(0)
+class BucketAggregator (bins: Array[Bucket]) extends Aggregator[Double, Array[Int], Array[Int]] {
+  override def default(): Array[Int] = Array.fill(bins.length)(0)
 
   override def finish(intermediate: Array[Int]): Array[Int] = intermediate
 
@@ -84,10 +150,15 @@ class BucketAggregator (minValue: Double, maxValue: Double, bins: Int) extends A
 
   override def add(current: Array[Int], next: Option[Double]): Array[Int] = {
     next.map{value =>
-      val bin = (bins * (value - minValue) / (maxValue - minValue)).floor.toInt
-      current(bin) = current(bin) + 1
+      val bin = bins.filter(b => b.withinBucket(value)).foreach(b => current(b.bucketIndex) = current(b.bucketIndex) + 1)
     }
     current
+  }
+}
+
+class Bucket(val lowerLimit: Double, val upperLimit: Double, val bucketIndex: Int) {
+  def withinBucket(value: Double): Boolean = {
+    value >= lowerLimit && value <= upperLimit
   }
 }
 
