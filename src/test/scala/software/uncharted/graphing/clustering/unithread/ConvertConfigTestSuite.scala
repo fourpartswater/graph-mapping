e5850cacc1f8c2f3848335d2922ed61756606602
Philippe Horne <phorne@uncharted.software>
2017-04-25 10:57:10 -0400

    Added some tests for the new configuration parsers.

---
+++ src/test/scala/software/uncharted/graphing/clustering/unithread/ConvertConfigTestSuite.scala
@@ -0,0 +1,32 @@
+/**
+  * Copyright (c) 2014-2016 Uncharted Software Inc. All rights reserved.
+  *
+  * Property of Uncharted(tm), formerly Oculus Info Inc.
+  * http://uncharted.software/
+  *
+  * This software is the confidential and proprietary information of
+  * Uncharted Software Inc. ("Confidential Information"). You shall not
+  * disclose such Confidential Information and shall use it only in
+  * accordance with the terms of the license agreement you entered into
+  * with Uncharted Software Inc.
+  */
+package software.uncharted.graphing.clustering.unithread
+
+import com.typesafe.config.ConfigFactory
+import org.scalatest.FunSuite
+
+class ConvertConfigTestSuite extends FunSuite {
+  test("Test parsing of configuration for convert step") {
+    val config = ConfigFactory.parseReader(scala.io.Source.fromURL(getClass.getResource("/convert-defaults.conf")).bufferedReader()).resolve()
+    val configParsed = ConvertConfigParser.parse(config).get
+    assert(configParsed.edgeInputFilename == "edges")
+    assert(configParsed.edgeOutputFilename == "edges.bin")
+    assert(configParsed.srcNodeColumn == 0)
+    assert(configParsed.dstNodeColumn == 1)
+    assert(configParsed.metaOutputFilename.get == "metadata.bin")
+    assert(configParsed.nodeInputFilename.get == "nodes")
+    assert(configParsed.nodeColumn == 1)
+    assert(configParsed.metaColumn == 0)
+    assert(configParsed.nodeAnalytics.length == 2)
+  }
+}
---
+++ src/test/scala/software/uncharted/graphing/export/ExportConfigTestSuite.scala
@@ -0,0 +1,27 @@
+/**
+  * Copyright (c) 2014-2016 Uncharted Software Inc. All rights reserved.
+  *
+  * Property of Uncharted(tm), formerly Oculus Info Inc.
+  * http://uncharted.software/
+  *
+  * This software is the confidential and proprietary information of
+  * Uncharted Software Inc. ("Confidential Information"). You shall not
+  * disclose such Confidential Information and shall use it only in
+  * accordance with the terms of the license agreement you entered into
+  * with Uncharted Software Inc.
+  */
+package software.uncharted.graphing.export
+
+import com.typesafe.config.ConfigFactory
+import org.scalatest.FunSuite
+
+class ExportConfigTestSuite extends FunSuite {
+  test("Test parsing of configuration for export step") {
+    val config = ConfigFactory.parseReader(scala.io.Source.fromURL(getClass.getResource("/export-defaults.conf")).bufferedReader()).resolve()
+    val configParsed = ExportConfigParser.parse(config).get
+    assert(configParsed.output == "output/nodes")
+    assert(configParsed.maxLevel == 6)
+    assert(configParsed.delimiter == "\t")
+    assert(configParsed.source == "source/layout")
+  }
+}
